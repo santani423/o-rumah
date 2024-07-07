25d689567662efa5b41208e29d31ab3c1fb8a220
Santani - <santani423@gmail.com>
2024-07-07 23:44:10 +0700

    type

--- resources/views/Pages/Frond/OFoodPage.blade.php
+++ resources/views/Pages/Frond/OFoodPage.blade.php
@@ -230,6 +230,12 @@
                     <x-Layout.Item.ProductItem :image="$ads->image" :title="$ads->title" :area="$ads->area" :jk="$ads->jk"
                         :price="$ads->price" :jkm="$ads->jkm" :lb="$ads->lb" :type="$ads->type" :lt="$ads->lt" :address="$ads->address"
                         :linkTujuan="route('ofood-detail', $ads->slug)">
+                        @if (floor($ads->distance) > 0)
+                <div class="card-link d-flex align-items-center mr-3">
+                    <i class="bi bi-geo-alt-fill"></i>
+                    <span class="ml-2">{{ floor($ads->distance) }} Km</span>
+                </div>
+            @endif
                     </x-Layout.Item.ProductItem>
 
                 </div><!-- end col -->

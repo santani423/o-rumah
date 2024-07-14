7ec0b73827444202346a3e11209d1a8088be909b
Santani - <santani423@gmail.com>
2024-07-14 07:34:23 +0700

    simpan

--- database/seeders/AdBalanceControlsTableSeeder.php
+++ database/seeders/AdBalanceControlsTableSeeder.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -13,17 +12,25 @@ class AdBalanceControlsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('ad_balace_controls')->insert([
-            ['code' => 'ABC001', 'title' => 'Ads KPR', 'klik' => 0, 'nilai' => 10, 'description' => 'Description for Ad Campaign 1', 'created_at' => now(), 'updated_at' => now()],
-            ['code' => 'ABC002', 'title' => 'Ads Titip', 'klik' => 0, 'nilai' => 3, 'description' => 'Description for Ad Campaign 2', 'created_at' => now(), 'updated_at' => now()],
-            ['code' => 'ABC003', 'title' => 'Ads Group Supplier', 'klik' => 0, 'nilai' => 0, 'description' => 'Description for Ad Campaign 3', 'created_at' => now(), 'updated_at' => now()],
-            ['code' => 'ABC004', 'title' => 'Ads Export Import', 'klik' => 0, 'nilai' => 0, 'description' => 'Description for Ad Campaign 4', 'created_at' => now(), 'updated_at' => now()],
-            ['code' => 'ABC005', 'title' => 'Ads Chart', 'klik' => 0, 'nilai' => 0, 'description' => 'Description for Ad Campaign 5', 'created_at' => now(), 'updated_at' => now()],
-            ['code' => 'ABC006', 'title' => 'Ads Projec Developr', 'klik' => 350, 'nilai' => 175, 'description' => 'Description for Ad Campaign 6', 'created_at' => now(), 'updated_at' => now()],
-            ['code' => 'ABC007', 'title' => 'Ads Klik', 'klik' => 10, 'nilai' => 1, 'description' => 'Description for Ad Campaign 7', 'created_at' => now(), 'updated_at' => now()],
-            ['code' => 'ABC008', 'title' => 'Ambil Lelang', 'klik' => 10, 'nilai' => 1, 'description' => 'Description for Ad Campaign 7', 'created_at' => now(), 'updated_at' => now()],
-        ]);
+        $adBalanceControls = [
+            ['code' => 'ABC001', 'title' => 'Ads KPR', 'klik' => 0, 'nilai' => 10, 'description' => 'Description for Ad Campaign 1'],
+            ['code' => 'ABC002', 'title' => 'Ads Titip', 'klik' => 0, 'nilai' => 3, 'description' => 'Description for Ad Campaign 2'],
+            ['code' => 'ABC003', 'title' => 'Ads Group Supplier', 'klik' => 0, 'nilai' => 0, 'description' => 'Description for Ad Campaign 3'],
+            ['code' => 'ABC004', 'title' => 'Ads Export Import', 'klik' => 0, 'nilai' => 0, 'description' => 'Description for Ad Campaign 4'],
+            ['code' => 'ABC005', 'title' => 'Ads Chart', 'klik' => 0, 'nilai' => 0, 'description' => 'Description for Ad Campaign 5'],
+            ['code' => 'ABC006', 'title' => 'Ads Project Developer', 'klik' => 350, 'nilai' => 175, 'description' => 'Description for Ad Campaign 6'],
+            ['code' => 'ABC007', 'title' => 'Ads Klik', 'klik' => 10, 'nilai' => 1, 'description' => 'Description for Ad Campaign 7'],
+            ['code' => 'ABC008', 'title' => 'Ambil Lelang', 'klik' => 10, 'nilai' => 1, 'description' => 'Description for Ad Campaign 8'],
+            ['code' => 'ABC009', 'title' => 'Titip Ads', 'klik' => 1, 'nilai' => 1, 'description' => 'Titip Ads'],
+        ];
 
-        //    php artisan db:seed --class=AdBalanceControlsTableSeeder
+        foreach ($adBalanceControls as $control) {
+            DB::table('ad_balace_controls')->updateOrInsert(
+                ['code' => $control['code']], // Search for this condition
+                array_merge($control, ['created_at' => now(), 'updated_at' => now()]) // Insert/update with this data
+            );
+        }
+
+        // To run: php artisan db:seed --class=AdBalanceControlsTableSeeder
     }
 }

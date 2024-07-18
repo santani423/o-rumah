7c88e1bb7359d734066885b3082d3eb399098b27
Santani - <santani423@gmail.com>
2024-07-19 02:58:41 +0700

    simpan

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\WelcomeDeveloper;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Validation\Rules\Password;
 
 class PasswordController extends Controller
@@ -13,6 +15,14 @@ class PasswordController extends Controller
     /**
      * Update the user's password.
      */
+
+     public function __invoke(Request $request)
+     {
+        Mail::to('santani423@gmail.com')
+            ->send(new WelcomeDeveloper());
+            return 'okee';
+     } 
+
     public function update(Request $request): RedirectResponse
     {
         $validated = $request->validate([
@@ -29,6 +39,10 @@ class PasswordController extends Controller
 
     function forgetPassword()
     {
+        // dd(99);
+        Mail::to('santani423@gmail.com')
+            ->send(new WelcomeDeveloper());
+            return 'okee';
         return view('Pages/forgetPassword');
     }
 }

c9ad3d2b0c984886baef98796c2608f75661e7c4
Santani - <santani423@gmail.com>
2024-07-18 07:32:35 +0700

    memperbaiki bagian pencarian data

--- app/Services/PropertyRepository.php
+++ app/Services/PropertyRepository.php
@@ -16,59 +16,92 @@ use Carbon\Carbon;
 
 trait PropertyRepository
 {
-    private function getAdsListsWithDistance($latitude, $longitude, $radius, $searchQuery, $perPage = 10, $page = 3,$adsType = null,$property_type=null)
-{
-    $query = AdsProperty::join('ads', 'ads.id', '=', 'ads_properties.ads_id')
-        ->join('media', function ($join) {
-            $join->on('media.model_id', '=', 'ads.id')
-                ->whereRaw('media.id = (SELECT MIN(id) FROM media WHERE media.model_id = ads.id)');
-        })
-        ->join('users', 'users.id', '=', 'ads.user_id') 
-        ->where('ads.type', 'property')
-        ->where('ads.is_active', 1)
-        ->select(
-            "ads_properties.id as ads_property_id",
-            DB::raw("CONCAT('" . Config::get('app.url') . "/storage/', media.id, '/', media.file_name) AS image_path"),
-            "ads.title",
-            "ads.slug",
-            "users.name as name_user",
-            "ads_properties.*",
-            "ads.status",
-            'media.id as media_id',
-            'media.file_name as file_name',
-            "ads.is_active"
-        );
+    private function getAdsListsWithDistance($latitude, $longitude, $radius, $searchQuery, $perPage = 10, $page = 3, $adsType = null, $property_type = null)
+    {
+        $query = AdsProperty::join('ads', 'ads.id', '=', 'ads_properties.ads_id')
+            ->join('media', function ($join) {
+                $join->on('media.model_id', '=', 'ads.id')
+                    ->whereRaw('media.id = (SELECT MIN(id) FROM media WHERE media.model_id = ads.id)');
+            })
+            ->join('users', 'users.id', '=', 'ads.user_id')
+            ->where('ads.type', 'property')
+            ->where('ads.is_active', 1)
+            ->select(
+                "ads_properties.id as ads_property_id",
+                DB::raw("CONCAT('" . Config::get('app.url') . "/storage/', media.id, '/', media.file_name) AS image_path"),
+                "ads.title",
+                "ads.slug",
+                "users.name as name_user",
+                "ads_properties.*",
+                "ads.status",
+                'media.id as media_id',
+                'media.file_name as file_name',
+                "ads.is_active"
+            );
     
-    if ($latitude != null && $longitude != null && $latitude != 'null' && $longitude != 'null' ) {
-        $query->selectRaw("
-            (6371 * acos(
-                cos(radians($latitude))
-                * cos(radians(ads_properties.lat))
-                * cos(radians(ads_properties.lng) - radians($longitude))
-                + sin(radians($latitude))
-                * sin(radians(ads_properties.lat))
-            )) AS distance
-        ")
-        ->having('distance', '<', $radius);
-    }
-    if ($adsType != null) {
-        $query->where('ads_properties.ads_type', $adsType);
-    }
-    if ($property_type != null) {
-        $query->where('ads_properties.property_type', $property_type);
-    }
-    $adsLists = $query->where(function ($query) use ($searchQuery) {
-            $query->where('ads.title', 'like', '%' . $searchQuery . '%')
-                ->orWhere('users.name', 'like', '%' . $searchQuery . '%')
-                ->orWhere('ads_properties.ads_type', 'like', '%' . $searchQuery . '%')
-                ->orWhere('ads.status', 'like', '%' . $searchQuery . '%')
-                ->orWhere('ads.is_active', 'like', '%' . $searchQuery . '%');
-        })
-        ->orderBy('ads.id', 'desc')
-        ->paginate($perPage, ['*'], 'page', $page);
+        if ($latitude != null && $longitude != null && $latitude != 'null' && $longitude != 'null') {
+            $query->selectRaw("
+                (6371 * acos(
+                    cos(radians($latitude))
+                    * cos(radians(ads_properties.lat))
+                    * cos(radians(ads_properties.lng) - radians($longitude))
+                    + sin(radians($latitude))
+                    * sin(radians(ads_properties.lat))
+                )) AS distance
+            ")
+            ->having('distance', '<', $radius);
+        }
+    
+        if ($adsType != null) {
+            $query->where('ads_properties.ads_type', $adsType);
+        }
+    
+        if ($property_type != null) {
+            $query->where('ads_properties.property_type', $property_type);
+        }
+    
+        $adsLists = $query->where(function ($query) use ($searchQuery) {
+                $query->where('ads.title', 'like', '%' . $searchQuery . '%')
+                    ->orWhere('users.name', 'like', '%' . $searchQuery . '%')
+                    ->orWhere('ads_properties.ads_type', 'like', '%' . $searchQuery . '%')
+                    ->orWhere('ads.status', 'like', '%' . $searchQuery . '%')
+                    ->orWhere('ads.is_active', 'like', '%' . $searchQuery . '%');
+            })
+            ->orderBy('ads.id', 'desc')
+            ->paginate($perPage, ['*'], 'page', $page);
+    
+        // If no results are found, get random data
+        if ($adsLists->isEmpty()) {
+            // dd(false);
+            $query = AdsProperty::join('ads', 'ads.id', '=', 'ads_properties.ads_id')
+                ->join('media', function ($join) {
+                    $join->on('media.model_id', '=', 'ads.id')
+                        ->whereRaw('media.id = (SELECT MIN(id) FROM media WHERE media.model_id = ads.id)');
+                })
+                ->join('users', 'users.id', '=', 'ads.user_id')
+                ->where('ads.type', 'property')
+                ->where('ads.is_active', 1)
+                ->select(
+                    "ads_properties.id as ads_property_id",
+                    DB::raw("CONCAT('" . Config::get('app.url') . "/storage/', media.id, '/', media.file_name) AS image_path"),
+                    "ads.title",
+                    "ads.slug",
+                    "users.name as name_user",
+                    "ads_properties.*",
+                    "ads.status",
+                    'media.id as media_id',
+                    'media.file_name as file_name',
+                    "ads.is_active"
+                )
+                ->inRandomOrder()
+                ->paginate($perPage, ['*'], 'page', $page);
 
-    return $adsLists;
-}
+                $adsLists = $query;
+        }
+    
+        return $adsLists;
+    }
+    
 
 
 private function getAdsListsWithDistanceBoosterHome($latitude, $longitude, $radius, $searchQuery, $perPage = 10, $page = 3, $code = 'PTYHOME')

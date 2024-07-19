f9b6abc9253d71913a4df5b5d9cd79bf1ed6d087
Santani - <santani423@gmail.com>
2024-07-19 21:52:51 +0700

    simpan

--- resources/views/Pages/Frond/Properti/detailProperti.blade.php
+++ resources/views/Pages/Frond/Properti/detailProperti.blade.php
@@ -61,69 +61,6 @@
     @endslot
 
     @slot('body')
-    <div class="col-lg-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title font-20 mt-0">
-                                    Carousel Card
-                                </h4>
-                                <p class="card-text">
-                                    This is a wider card with supporting text
-                                    below as a natural lead-in to additional
-                                    content. This content is a little bit
-                                    longer.
-                                </p>
-                                <p class="card-text">
-                                    <small class="text-muted"
-                                        >Last updated 3 mins ago</small
-                                    >
-                                </p>
-                            </div>
-                            <div
-                                id="carouselExampleControls"
-                                class="carousel slide"
-                                data-ride="carousel"
-                            >
-                                <div class="carousel-inner">
-                                    
-                                 @foreach($media as $key => $md)
-                                    <div class="carousel-item @if($key == 0) active @endif">
-                                        <img
-                                            class="d-block w-100"
-                                            src="{{ asset($md['url']) }}"
-                                            alt="First slide"
-                                             style="max-width: 800px; height: auto;"
-                                        />
-                                    </div>
-                                    @endforeach
-                                </div>
-                                <a
-                                    class="carousel-control-prev"
-                                    href="#carouselExampleControls"
-                                    role="button"
-                                    data-slide="prev"
-                                >
-                                    <span
-                                        class="carousel-control-prev-icon"
-                                        aria-hidden="true"
-                                    ></span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                                <a
-                                    class="carousel-control-next"
-                                    href="#carouselExampleControls"
-                                    role="button"
-                                    data-slide="next"
-                                >
-                                    <span
-                                        class="carousel-control-next-icon"
-                                        aria-hidden="true"
-                                    ></span>
-                                    <span class="sr-only">Next</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
     <div class="row">
         <div class="col-lg-12">
             <div class="card">
--- resources/views/components/layout/item/real-estate-photo-viewer.blade.php
+++ resources/views/components/layout/item/real-estate-photo-viewer.blade.php
@@ -36,7 +36,8 @@
             <div class="carousel-inner" role="listbox">
                 @foreach($media as $key => $md)
                     <div class="carousel-item @if($key == 0) active @endif">
-                        <img class="d-block img-fluid" src="{{ asset($md['url']) }}" alt="First slide">
+                        <img class="d-block img-fluid" src="{{ asset($md['url']) }}" alt="First slide" 
+                        style="max-width: 800px; height: auto;">
                     </div>
                 @endforeach
             </div>

bc7a716354024e802e4d6e96195336b59ea0c99c
Santani - <santani423@gmail.com>
2024-07-27 11:27:43 +0700

    simpan

--- routes/web.php
+++ routes/web.php
@@ -339,7 +339,7 @@ Route::get('/titip-ads/list', [TitipAdsController::class, 'index'])->name('titip
 
 
 
-Route::get('/forget-password', [PasswordController::class, 'forgetPassword'])->name('forget.password');
+Route::get('/forget-password', [PasswordController::class, 'forgetPassword'])->name('forget.passwrod');
 // Route::post('/forget-password-email', [MailController::class, 'forgetPassword'])->name('forget.passwrod.email');
 Route::get('/forget-password-email', [MailController::class, 'forgetPassword'])->name('forget.passwrod.email');
 Route::get('/password-change/{uuid}', [PasswordChangesController::class, 'show'])->name('passwrod.change');

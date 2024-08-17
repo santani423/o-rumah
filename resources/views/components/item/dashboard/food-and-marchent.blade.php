eefbee943196b05b73aaeb80db01892a90abfc82
Santani - <santani423@gmail.com>
2024-08-17 19:35:03 +0700

    simpan

---
+++ resources/views/components/item/dashboard/food-and-marchent.blade.php
@@ -0,0 +1,86 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-lg-3 col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-row">
+                            <div class="col-3 align-self-center">
+                                <div class="round">
+                                    <i class="mdi mdi-eye"></i>
+                                </div>
+                            </div>
+                            <div class="col-9 align-self-center text-right">
+                                <div class="m-l-10">
+                                    <h5 class="mt-0">Plan</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!--end card-body-->
+                </div><!--end card-->
+            </div><!--end col-->
+
+            <div class="col-lg-3 col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-row">
+                            <div class="col-3 align-self-center">
+                                <div class="round">
+                                    <i class="mdi mdi-food-fork-drink"></i>
+                                </div>
+                            </div>
+                            <div class="col-9 align-self-center text-right">
+                                <div class="m-l-10">
+                                    <h5 class="mt-0">18090</h5>
+                                    <!-- <p class="mb-0 text-muted">Food</p> -->
+                                    <p class="mb-0 text-muted">Tunda: 2, Tayang: 10</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!--end card-body-->
+                </div><!--end card-->
+            </div><!--end col-->
+
+            <div class="col-lg-3 col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-row">
+                            <div class="col-3 align-self-center">
+                                <div class="round">
+                                    <i class="mdi mdi-store"></i>
+                                </div>
+                            </div>
+                            <div class="col-9 text-right align-self-center">
+                                <div class="m-l-10 ">
+                                    <h5 class="mt-0">562</h5>
+                                    <!-- <p class="mb-0 text-muted">Merchant</p> -->
+                                    <p class="mb-0 text-muted">Tunda: 3, Tayang: 8</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!--end card-body-->
+                </div><!--end card-->
+            </div><!--end col-->
+
+            <div class="col-lg-3 col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-row">
+                            <div class="col-3 align-self-center">
+                                <div class="round ">
+                                    <i class="mdi mdi-cart"></i>
+                                </div>
+                            </div>
+                            <div class="col-9 align-self-center text-right">
+                                <div class="m-l-10 ">
+                                    <h5 class="mt-0">7514</h5>
+                                    <p class="mb-0 text-muted">Booster</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!--end card-body-->
+                </div><!--end card-->
+            </div><!--end col-->
+        </div><!--end row-->
+    </div><!--end col-->
+</div><!--end row-->

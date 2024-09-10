2a71bdc7651e7d5d7a0e81504defbd9bb663608c
Santani - <santani423@gmail.com>
2024-09-11 05:29:26 +0700

    simpan

--- resources/views/components/tool/user-property-statistics.blade.php
+++ resources/views/components/tool/user-property-statistics.blade.php
@@ -3,7 +3,7 @@
         <div class="card rounded-0 border border-1">
             <div class="card-body text-center pr-2 pl-2">
                 <p class="small">Properti</p>
-                <h6 class="mt-0 font-16">{{ $totalProperties }}</h6>
+                <h6 class="mt-0" style="  font-size: 70%;">{{ $totalProperties }}</h6>
             </div>
         </div>
     </div>
@@ -11,7 +11,7 @@
         <div class="card rounded-0 border border-1">
             <div class="card-body text-center pr-2 pl-2">
                 <p class="small">Terjual</p>
-                <h6 class="mt-0 font-16">{{ $total_sold_properties }}</h6>
+                <h6 class="mt-0" style="  font-size: 70%;">{{ $total_sold_properties }}</h6>
             </div>
         </div>
     </div>
@@ -19,7 +19,7 @@
         <div class="card rounded-0 border border-1 pr-0 pl-0 mr-0 ml-0">
             <div class="card-body text-center">
                 <p class="small">Tersewa</p>
-                <h6 class="mt-0 font-16">{{ $total_rented_properties }}</h6>
+                <h6 class="mt-0" style="  font-size: 70%;">{{ $total_rented_properties }}</h6>
             </div>
         </div>
     </div>
